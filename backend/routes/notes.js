74274bd9e41b9229042d1d826e049b59eb605e2e
NIKHIL AGRAWAL <nagrawal203@gmail.com>
2023-12-01 15:13:00 +0530

    Vid53Comp

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,4 +49,38 @@ router.post(
   }
 );
 
+// ROUTE 3: Update an existing Note using: POST "/api/notes/updatenote". Login required
+router.post("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+
+    //Create a newNote object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //Find the note to be updated
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      res.status(404).send("Not Found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json({note});
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
