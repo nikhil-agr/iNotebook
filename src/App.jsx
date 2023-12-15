63513b025ae4bc27f2d1cf9e49e8235af4dce6a7
NIKHIL AGRAWAL <nagrawal203@gmail.com>
2023-12-15 11:50:10 +0530

    Removed Alert

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
-import Alert from "./components/Alert/Alert";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Alert message="This is Amazing" />
       <Outlet />
     </>
   );
